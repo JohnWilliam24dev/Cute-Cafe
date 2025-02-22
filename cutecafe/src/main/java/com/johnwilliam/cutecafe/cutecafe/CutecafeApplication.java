707ced43b48059a9be692dfc415f11e8b9d9c855
Seu Nome <seu@email.com>
2025-02-22 18:38:24 -0300

    inicio

---
+++ cutecafe/src/main/java/com/johnwilliam/cutecafe/cutecafe/CutecafeApplication.java
@@ -0,0 +1,13 @@
+package com.johnwilliam.cutecafe.cutecafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CutecafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CutecafeApplication.class, args);
+	}
+
+}
---
+++ cutecafe/src/test/java/com/johnwilliam/cutecafe/cutecafe/CutecafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johnwilliam.cutecafe.cutecafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CutecafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
